d51834d94833b2a5554f344564538db3bce4d30d
leotran671 <leo.tran@my-digital-school.org>
2024-06-17 15:29:10 +0200

    refacto schema

---
+++ back/src/model.ts
@@ -0,0 +1,23 @@
+export type User = {
+    id: string
+    username: String
+}
+
+export type Post = {
+    id: string
+    content: String
+    user: User
+    authorId: String
+    authorName: String
+    comments: Comment[]
+    likes: User[]
+}
+
+export type Comment = {
+    id: string
+    content: String
+    user: User
+    authorId: String
+    authorName: String
+    postId: String
+}
