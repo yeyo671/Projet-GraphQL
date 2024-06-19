92253bcd86fc1b1686712bdabb690af148507053
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-06-19 16:09:53 +0200

    refactor: Update App component to use react-router-dom for routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import HomePage from "./components/pages/HomePage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from "./components/pages/RegisterPage";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<RegisterPage />} />
+        <Route
+          path="/*"
+          element={
+            <>
+              <Header />
+              <HomePage />
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
