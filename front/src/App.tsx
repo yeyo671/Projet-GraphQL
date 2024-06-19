d8ed0ba55ac3f36f3173f261c2b90c93699b0774
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-06-19 15:57:29 +0200

    chore: Add react-router-dom package for routing in App component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/layout/Header";
 import HomePage from "./components/pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ front/src/components/pages/RegisterPage.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      {/* Add your registration form here */}
+    </div>
+  );
+}
