fa9223822214107a8275018e1dee3cb3bf9af5f4
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-06-19 15:20:30 +0200

    refactor: Update PostCard component to use GraphQL query types

--- front/src/components/feed/PostCard.tsx
+++ front/src/components/feed/PostCard.tsx
@@ -1,14 +1,17 @@
-type PostCardProps = {
-  post: {
-    content: string;
-    authorName: string;
-  };
-};
+import { GetPostQuery } from "../../gql/graphql";
+
+interface PostCardProps {
+  post: GetPostQuery["getPost"];
+}
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <div className="bg-base-100 rounded-btn p-4">
-      <h2 className="text-xl font-bold mb-2">Post Title</h2>
+      <div className="avatar placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-10">
+          <span className="text-sm">{post.authorName[0].toUpperCase()}</span>
+        </div>
+      </div>
       <p className="text-gray-500 mb-2">{post.authorName}</p>
       <p className="text-gray-700">{post.content}</p>
     </div>
