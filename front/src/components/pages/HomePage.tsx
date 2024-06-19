2ab6af7e81caea751e3f874d5a2377d357ae960f
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-06-19 15:28:51 +0200

    chore: Update HomePage component to include min-height property

--- front/src/components/pages/HomePage.tsx
+++ front/src/components/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import Feed from "../feed/Feed";
 
 export default function HomePage() {
   return (
-    <div className="bg-base-200">
+    <div className="bg-base-200 min-h-screen">
       <Feed />
     </div>
   );
