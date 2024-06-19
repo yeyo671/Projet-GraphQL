4fea42952347cc44c9069e0b7f41765f9e769c9f
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-06-19 14:43:49 +0200

    chore: Update Feed component to use Apollo Client for data fetching

--- front/src/components/feed/Feed.tsx
+++ front/src/components/feed/Feed.tsx
@@ -1,97 +1,27 @@
 import CommentForm from "./CommentForm";
 import PostCard from "./PostCard";
 
-const posts = [
-  {
-    id: 1,
-    user: {
-      name: "Dimitri Fiacre",
-      avatar: "https://avatar.iran.liara.run/public",
-    },
-    timestamp: "Il y a 1 heure",
-    content: "Overwatch 2 va sortir le 4 octobre 2022, j'ai trop hâte !",
-    image: "path/to/image.jpg",
-    comments: [
-      {
-        user: {
-          name: "Marc",
-          avatar: "https://avatar.iran.liara.run/public",
-        },
-        content: "Quel est ce jeu ?",
-      },
-      {
-        user: {
-          name: "Mbappe",
-          avatar: "https://avatar.iran.liara.run/public",
-        },
-        content: "Overwatch il a changé",
-      },
-    ],
-  },
-  {
-    id: 2,
-    user: {
-      name: "Dimitri Fiacre",
-      avatar: "https://avatar.iran.liara.run/public",
-    },
-    timestamp: "Il y a 2 heures",
-    content:
-      "Je viens de finir la saison 3 de The Mandalorian, c'était génial !",
-    image: "path/to/image2.jpg",
-    comments: [
-      {
-        user: {
-          name: "Marc",
-          avatar: "path/to/avatar2.jpg",
-        },
-        content: "Je suis d'accord !",
-      },
-      {
-        user: {
-          name: "Mbappe",
-          avatar: "path/to/avatar3.jpg",
-        },
-        content: "Je n'ai pas encore regardé",
-      },
-    ],
-  },
-  {
-    id: 3,
-    user: {
-      name: "Dimitri Fiacre",
-      avatar: "path/to/avatar.jpg",
-    },
-    timestamp: "Il y a 3 heures",
-    content: "Je viens de commencer le livre Dune, c'est vraiment bien écrit !",
-    image: "path/to/image3.jpg",
-    comments: [
-      {
-        user: {
-          name: "Marc",
-          avatar: "path/to/avatar2.jpg",
-        },
-        content: "Je l'ai lu il y a quelques années, c'est un classique !",
-      },
-      {
-        user: {
-          name: "Mbappe",
-          avatar: "path/to/avatar3.jpg",
-        },
-        content: "Je ne suis pas très science-fiction",
-      },
-    ],
-  },
-];
+import { GetPostsDocument } from "../../gql/graphql";
+import { useQuery } from "@apollo/client";
 
 export default function Feed() {
+  const { data, loading, error } = useQuery(GetPostsDocument);
+  if (error) return <p>Error :</p>;
+
   return (
     <>
       <div className="container mx-auto pt-4">
         <div className="flex flex-col gap-4">
           <CommentForm />
-          {posts.map((post) => (
-            <PostCard key={post.id} post={post} />
-          ))}
+          {loading ? (
+            <div className="flex justify-center">
+              <span className="loading loading-spinner text-primary"></span>
+            </div>
+          ) : (
+            data?.getPosts.map(
+              (post) => post && <PostCard key={post.id} post={post} />
+            )
+          )}
         </div>
       </div>
     </>
--- front/src/components/feed/PostCard.tsx
+++ front/src/components/feed/PostCard.tsx
@@ -1,25 +1,15 @@
-import React from "react";
-
 type PostCardProps = {
   post: {
     content: string;
-    user: {
-      name: string;
-      avatar: string;
-    };
+    authorName: string;
   };
 };
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <div className="bg-base-100 rounded-btn p-4">
-      <div className="avatar">
-        <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src={post.user.avatar} />
-        </div>
-      </div>
       <h2 className="text-xl font-bold mb-2">Post Title</h2>
-      <p className="text-gray-500 mb-2">{post.user.name}</p>
+      <p className="text-gray-500 mb-2">{post.authorName}</p>
       <p className="text-gray-700">{post.content}</p>
     </div>
   );
