196a86f0285b5eef96bfaf277167230d1e3c2e2c
yeyo671 <leotran671@gmail.com>
2024-06-23 01:01:12 +0200

    'comment' is possibly 'null'

--- front/src/components/feed/CommentsDisplay.tsx
+++ front/src/components/feed/CommentsDisplay.tsx
@@ -11,18 +11,18 @@ const CommentsDisplay: React.FC<CommentsDisplayProps> = ({ comments }) => {
     <div className="comments-section flex flex-col gap-1">
       {comments.map((comment) => {
         return (
-          <div key={comment.id} className="comment">
+          <div key={comment?.id} className="comment">
             <div className="flex flex-row gap-2 items-center">
               <div className="avatar placeholder">
                 <div className="bg-neutral text-neutral-content rounded-full w-8 h-8">
                   <span className="text-sm">
-                    {comment.authorName[0].toUpperCase()}
+                    {comment?.authorName[0].toUpperCase()}
                   </span>
                 </div>
               </div>
               <div>
-                <div className="post-author text-sm">{comment.authorName}</div>
-                <div className="post-date text-xs">{comment.content}</div>
+                <div className="post-author text-sm">{comment?.authorName}</div>
+                <div className="post-date text-xs">{comment?.content}</div>
               </div>
             </div>
           </div>
